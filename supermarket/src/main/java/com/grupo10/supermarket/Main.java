5adbe3e66dbbd6f3120fdf7a39875af94b5bf1f7
Jeysson Guevara <guevara@civicplus.com>
2023-07-21 21:13:43 -0500

    Creada interfaz de usuario.

---
+++ supermarket/src/main/java/com/grupo10/supermarket/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupo10.supermarket;
+
+import com.grupo10.supermarket.views.MainWindow;
+
+/**
+ *
+ * @author jeiss
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        new MainWindow().setVisible(true);
+        
+        System.out.println("Hello World!");
+    }
+}
